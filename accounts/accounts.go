5dc39d31c2fe996bf3746b0f254f9878056da620
Yongbbbba <lisagamer123@gmail.com>
2020-07-09 20:53:53 +0900

    practice making method through some example before making url checker or something

--- accounts/accounts.go
+++ accounts/accounts.go
@@ -1,5 +1,9 @@
 package accounts
 
+import "errors"
+
+//struct는 python의 class와 object를 합친 것과 비슷
+
 // Account struct
 type Account struct {
 	owner   string
@@ -10,4 +14,40 @@ type Account struct {
 func NewAccount(owner string) *Account {
 	account := Account{owner: owner, balance: 0}
 	return &account
+}
+
+// Deposit x amount on your account
+// a Account는 receiver 라고 함
+func (a *Account) Deposit(amount int) {
+	a.balance += amount
+}
+
+// Balance of your account
+func (a Account) Balance() int {
+	return a.balance
+
+}
+
+var errNoMoney = errors.New("Can't wtihdraw, you are poor")
+
+// Withdraw x amount from your account
+// error handling
+func (a *Account) Withdraw(amount int) error {
+	if a.balance < amount {
+		return errNoMoney
+	}
+	a.balance -= amount
+	return nil
+}
+
+// Change Owner of the account
+func (a *Account) ChangeOwner(newOwner string) {
+	a.owner = newOwner
+
+}
+
+// Owner of the acocunt
+func (a Account) Owner() string {
+	return a.owner
+
 }
